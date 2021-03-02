7edcdfde2e98e73166dda78612f3bf8d9352693f
Yuehao <pandaxfly@gmail.com>
2021-03-02 22:35:09 +0800

    deprecated project as solution moved to Cloud Run

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const formidable = require('formidable');
 
-app.post('/api/upload', (req, res, next) => {
+app.post('/api/upload', (req, res) => {
   // Imports Google Cloud client library
   const vision = require('@google-cloud/vision');
 
@@ -14,22 +14,23 @@ app.post('/api/upload', (req, res, next) => {
   const form = formidable({ multiples: false });
 
   let fileBuffer = null;
-  
+
   form.onPart = (part) => {
     part.on('data', (buffer) => {
+      console.log(part.mime)
       if (part.mime) {
         fileBuffer = Buffer.from(buffer);
       }
     });
   };
-
+  
   const myPromise = new Promise ((resolve, reject) => {
 
     // Set response headers
     res.set('Access-Control-Allow-Origin', "*");
     res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.set('Access-Control-Request-Headers', 'Content-Type');
-
+    
     const returnedReqestBody = {
       image: { content: null },
       // All features can be find here
@@ -38,11 +39,11 @@ app.post('/api/upload', (req, res, next) => {
       //   {type: 'DOCUMENT_TEXT_DETECTION'}
       // ],
     };
-
+    
     form.parse(req, (err) => {
       // Set promise state to Reject
       if (err)  reject(err);
-
+        
       // Construct request body for Vision API
       returnedReqestBody.image.content = fileBuffer;
       console.log('returned request body:: ', returnedReqestBody);
@@ -65,19 +66,5 @@ app.post('/api/upload', (req, res, next) => {
     res.status(500).send(error);
   });
 });
- 
-app.listen(8081, () => {
-  console.log('Server listening on http://localhost:8081 ...');
-});
 
-
-exports.detectText = (cloudFunctionRequest, res) => {
-
-  const AUTH_API = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-
-  console.log('content-type::', cloudFunctionRequest.get('content-type'));
-  console.log('body:: ', cloudFunctionRequest.body);
-
-  res.status(200).send('SUCCESS');
-
-};
+exports.detectText = app;
